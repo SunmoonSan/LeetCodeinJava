c52bdfc7b529426ce3d064e0b037167170ec531c
San <18814184179@163.com>
2018-08-24 00:07:06 +0800

    202. 快乐数

---
+++ src/com/maple/leetcode/code200/LeetCode202.java
@@ -0,0 +1,57 @@
+package com.maple.leetcode.code200;
+/*
+编写一个算法来判断一个数是不是“快乐数”。
+一个“快乐数”定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是无限循环但始终变不到 1。如果可以变为 1，那么这个数就是快乐数。
+示例:
+输入: 19
+输出: true
+解释:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+*/
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode202 {
+    public boolean isHappy(int n) {
+
+        if (n < 1) {
+            return false;
+        }
+
+        Set<Integer> set = new HashSet<>();
+
+        while (true) {
+            int total = 0;  // 计算一次分解后的和
+
+            while (n > 0) {
+                int r = n % 10;
+                total += r * r;
+                n = n / 10;
+            }
+
+            // 分解后和为1,则返回true
+            if (total == 1) {
+                return true;
+            }
+
+            // 不等于1时,查找集合中是否存在,若存在则会死循环,必然不是快乐数
+            if (set.contains(total)) {
+                return false;
+            } else {
+                set.add(total);
+                n = total;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        LeetCode202 leet = new LeetCode202();
+        boolean b = leet.isHappy(19);
+        System.out.println(b);
+    }
+}
