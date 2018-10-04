0cb02984c5ba2fdecc19a07c69d5b45efe68c490
San <18814184179@163.com>
2018-10-04 12:47:20 +0800

    ListNode添加数组参数构造方法

--- src/com/maple/leetcode/code000/ListNode.java
+++ src/com/maple/leetcode/code000/ListNode.java
@@ -7,4 +7,30 @@ public class ListNode {
     ListNode(int x) {
         val = x;
     }
+
+    public ListNode(int[] array) {
+        if (array == null || array.length == 0) {
+            throw new IllegalArgumentException("Array can not be empty!");
+        }
+
+        this.val = array[0];
+        ListNode current = this;
+        for (int i = 1; i < array.length; i++) {
+            current.next = new ListNode(array[i]);
+            current = current.next;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        ListNode current = this;
+        while (current != null) {
+            res.append(current.val).append("->");
+            current = current.next;
+        }
+        res.append("null");
+
+        return res.toString();
+    }
 }
