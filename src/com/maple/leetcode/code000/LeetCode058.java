e1caa32fda7b0cd790ccc7050aa947a59f34f205
San <18814184179@163.com>
2018-10-12 23:12:55 +0800

    58. 最后一个单词的长度

--- src/com/maple/leetcode/code000/LeetCode058.java
+++ src/com/maple/leetcode/code000/LeetCode058.java
@@ -1,5 +1,4 @@
 package com.maple.leetcode.code000;
-
 /*
  给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
  如果不存在最后一个单词，请返回 0 。
@@ -9,19 +8,19 @@ package com.maple.leetcode.code000;
 
  输入: "Hello World"
  输出: 5
- */
+*/
 
 public class LeetCode058 {
     public int lengthOfLastWord(String s) {
 
-        int count=0; // 统计最后一个单词的字符数
-        for (int i=s.length()-1;i>=0;i--) {
-            if (s.charAt(i)==' ') {
+        int count = 0; // 统计最后一个单词的字符数
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
                 continue;
             }
 
             // i>count 放前面, 否则 "a" 这样的字符串会越界
-            while (i>=count && s.charAt(i-count)!=' ') {
+            while (i >= count && s.charAt(i - count) != ' ') {
                 count++;
             }
 
