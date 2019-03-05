3fd8d7903a8ddc6392c6c19b954bfe2e7c2559fc
San <18814184179@163.com>
2019-03-05 09:21:28 +0800

    73. 矩阵置零

---
+++ src/com/maple/leetcode/code000/LeetCode073.java
@@ -0,0 +1,82 @@
+package com.maple.leetcode.code000;
+/*
+给定一个 m x n 的矩阵，如果一个元素为 0，则将其所在行和列的所有元素都设为 0。请使用原地算法。
+
+示例 1:
+输入:
+[
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]
+]
+输出:
+[
+  [1,0,1],
+  [0,0,0],
+  [1,0,1]
+]
+
+示例 2:
+输入:
+[
+  [0,1,2,0],
+  [3,4,5,2],
+  [1,3,1,5]
+]
+输出: 
+[
+  [0,0,0,0],
+  [0,4,5,0],
+  [0,3,1,0]
+]
+进阶:
+一个直接的解决方案是使用  O(mn) 的额外空间，但这并不是一个好的解决方案。
+一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
+你能想出一个常数空间的解决方案吗？
+ */
+
+public class LeetCode073 {
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int[] rows = new int[row];
+        int[] cols = new int[col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = 1;
+                    cols[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < rows.length; i++) {
+            if (rows[i] == 1) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < cols.length; i++) {
+            if (cols[i] == 1) {
+                for (int j = 0; j < matrix.length; j++) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        LeetCode073 leet = new LeetCode073();
+        int[][] arr = {
+                {0, 1, 2, 0},
+                {3, 4, 5, 2},
+                {1, 3, 1, 5}
+        };
+        leet.setZeroes(arr);
+    }
+}
