bf7f9b2b53dfa379ff1ff8d10a5ae9a6d4b7d43f
San <18814184179@163.com>
2019-03-06 09:21:31 +0800

    102. 二叉树的层次遍历

--- src/com/maple/leetcode/code100/LeetCode102.java
+++ src/com/maple/leetcode/code100/LeetCode102.java
@@ -1,4 +1,21 @@
 package com.maple.leetcode.code100;
+/*
+给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+例如:
+给定二叉树: [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层次遍历结果：
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+ */
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -6,5 +23,41 @@ import java.util.List;
 import java.util.Queue;
 
 public class LeetCode102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<>();
+        if (root == null) {
+            return lists;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<TreeNode> treeNodes = new ArrayList<>();
+            while (!queue.isEmpty()) {
+                treeNodes.add(queue.remove());
+            }
+
+            List<Integer> integers = new ArrayList<>();
+            for (TreeNode tree : treeNodes) {
+                integers.add(tree.val);
+
+                if (tree.left != null) {
+                    queue.add(tree.left);
+                }
+
+                if (tree.right != null) {
+                    queue.add(tree.right);
+                }
+
+            }
+
+            lists.add(integers);
+        }
+        return lists;
+    }
+
+    public static void main(String[] args) {
+        LeetCode102 leet = new LeetCode102();
+    }
 
 }
