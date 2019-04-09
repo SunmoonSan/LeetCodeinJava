42cd02ceac4b0df5dfc00b7fa41979cbfa5b8ccc
San <18814184179@163.com>
2019-04-09 09:42:16 +0800

    347. 前K个高频元素

---
+++ src/com/maple/leetcode/code300/LeetCode347.java
@@ -0,0 +1,53 @@
+package com.maple.leetcode.code300;
+/*
+给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+
+示例 1:
+输入: nums = [1,1,1,2,2,3], k = 2
+输出: [1,2]
+
+示例 2:
+输入: nums = [1], k = 1
+输出: [1]
+说明：
+你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+ */
+
+import java.util.*;
+
+public class LeetCode347 {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            if (map.containsKey(n)) {
+                map.put(n, map.get(n) + 1);
+            } else {
+                map.put(n, 1);
+            }
+        }
+
+        Queue<Integer> queue = new PriorityQueue<>((o1, o2) -> map.get(o1) - map.get(o2));
+
+        for (Integer key : map.keySet()) {
+            queue.add(key);
+            if (queue.size() > k) {
+                queue.remove();
+            }
+        }
+
+        List<Integer> integers = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            integers.add(queue.remove());
+        }
+
+        return integers;
+    }
+
+    public static void main(String[] args) {
+        LeetCode347 leet = new LeetCode347();
+        int[] nums = {1, 1, 1, 2, 2, 3, 3, 3};
+        int k = 2;
+        leet.topKFrequent(nums, k);
+    }
+}
