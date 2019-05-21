d98800fceda8c967effef27d7078b019c38f69cd
San <18814184179@163.com>
2019-05-21 10:49:54 +0800

    225. 用队列实现栈

---
+++ src/com/maple/leetcode/code200/LeetCode225.java
@@ -0,0 +1,90 @@
+package com.maple.leetcode.code200;
+/*
+ * @desc : Created by San on 2019-05-03 07:36
+ */
+/*
+使用队列实现栈的下列操作：
+
+push(x) -- 元素 x 入栈
+pop() -- 移除栈顶元素
+top() -- 获取栈顶元素
+empty() -- 返回栈是否为空
+注意:
+
+你只能使用队列的基本操作-- 也就是 push to back, peek/pop from front, size, 和 is empty 这些操作是合法的。
+你所使用的语言也许不支持队列。 你可以使用 list 或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+你可以假设所有操作都是有效的（例如, 对一个空的栈不会调用 pop 或者 top 操作）。
+ */
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LeetCode225 {
+
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+        stack.push(0);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.top());
+    }
+}
+
+class MyStack {
+
+    private LinkedList<Integer> queue;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    /**
+     * Push element x onto stack.
+     */
+    public void push(int x) {
+        queue.addLast(x);
+    }
+
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
+    public int pop() {
+        return queue.removeLast();
+    }
+
+    /**
+     * Get the top element.
+     */
+    public int top() {
+        return queue.getLast();
+    }
+
+    /**
+     * Returns whether the stack is empty.
+     */
+    public boolean empty() {
+        return queue.size() == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("[");
+        for (int i = 0; i < queue.size(); i++) {
+            res.append(queue.get(i));
+            if (i != queue.size() - 1) {
+                res.append(", ");
+            }
+        }
+        res.append("]");
+        return res.toString();
+    }
+}
+
