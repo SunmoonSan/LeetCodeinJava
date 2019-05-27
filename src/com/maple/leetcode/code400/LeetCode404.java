f8589b825278f688a17f44983f79b0cb9c6823ec
San <18814184179@163.com>
2019-05-27 09:10:58 +0800

    404. 左叶子之和

--- src/com/maple/leetcode/code400/LeetCode404.java
+++ src/com/maple/leetcode/code400/LeetCode404.java
@@ -1,20 +1,38 @@
 package com.maple.leetcode.code400;
 
+/*
+计算给定二叉树的所有左叶子之和。
+
+示例：
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ */
+
 public class LeetCode404 {
     public int sumOfLeftLeaves(TreeNode root) {
-        int sum = 0;
         if (root == null) {
             return 0;
         }
 
-        if (root.left == null && root.right == null) {
-            return root.val;
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            return root.left.val + sumOfLeftLeaves(root.right);
+        } else {
+            return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
         }
+    }
 
-        if (root.left!=null && root.right==null) {
-            return sum+sumOfLeftLeaves(root.left);
-        }
-
-        return sum + sumOfLeftLeaves(root.right) + sumOfLeftLeaves(root.right);
+    public static void main(String[] args) {
+        LeetCode404 leet = new LeetCode404();
+        Integer[] arr = {3, 9, 20, 8, 7, 15, 7};
+        BinaryTree bt = new BinaryTree(arr);
+        System.out.println(bt);
+        int s = leet.sumOfLeftLeaves(bt.root);
+        System.out.println(s);
     }
 }
