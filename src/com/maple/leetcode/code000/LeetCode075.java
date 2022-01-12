a69ed81f4c7a1180b377ee1d0fc39869473f512a
SunmoonSan <18814184179@163.com>
2022-01-13 00:25:20 +0800

    75. 颜色分类

--- src/com/maple/leetcode/code000/LeetCode075.java
+++ src/com/maple/leetcode/code000/LeetCode075.java
@@ -2,26 +2,34 @@ package com.maple.leetcode.code000;
 
 public class LeetCode075 {
     public void sortColors(int[] nums) {
-        int l = -1;
-        int r = nums.length;
+        int t = -1;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                l++;
-                swap(nums, l, i);
-            }
-            if (nums[i] == 1) {
-
-            }
-            if (nums[i] == 2) {
-                r--;
-                while (l < r && nums[r] == 2) {
-                    r--;
+            if (nums[i] != 0) {
+                int j = i + 1;
+                while (j < nums.length && nums[j] != 0) {
+                    j++;
                 }
-                swap(nums, i, r);
+                if (j < nums.length && nums[j] == 0) {
+                    t = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = t;
+                }
             }
         }
 
-        System.out.println(nums);
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] != 2) {
+                int j = i - 1;
+                while (j >= 0 && nums[j] != 2) {
+                    j--;
+                }
+                if (j >= 0 && nums[j] == 2) {
+                    t = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = t;
+                }
+            }
+        }
     }
 
     private void swap(int[] nums, int a, int b) {
